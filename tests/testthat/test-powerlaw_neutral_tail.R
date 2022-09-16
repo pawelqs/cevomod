a4de96c34c2390ced716f5f4d316f66f1b5b7f31
Paweł <kpawel2210@gmail.com>
2022-09-16 18:32:53 +0200

    calc_powerlaw_curve now scales the parameter using the SFS binwidth

--- tests/testthat/test-powerlaw_neutral_tail.R
+++ tests/testthat/test-powerlaw_neutral_tail.R
@@ -7,12 +7,12 @@ lm <- fit_neutral_lm(mf1f)
 test_that("calc_powerlaw_curve works", {
   curve <- lm |>
     slice(1) |>
-    calc_powerlaw_curve()
+    calc_powerlaw_curve(binwidth = 0.01)
   expect_equal(nrow(curve), 100)
-  expect_equal(curve$n[1:5], c(18984, 4746, 2109, 1187, 759), tolerance = 0.1)
+  expect_equal(curve$n[1:5], c(17085.6315, 4271.4079, 1898.4035, 1067.8520, 683.4253), tolerance = 0.1)
   expect_equal(
     curve$n[95:100],
-    c(2.103494, 2.059900, 2.017646, 1.976680, 1.936949, 1.898404),
+    c(1.893145, 1.853910, 1.815882, 1.779012, 1.743254, 1.708563),
     tolerance = 0.0001
   )
   expect_true(all(c("neutr", "n") %in% names(curve)))
@@ -32,7 +32,7 @@ test_that("estimate_sampling_rate returns proper tibble", {
   expect_true(all(c("err", "sampling_rate") %in% names(sampling_rate)))
   expect_equal(
     sampling_rate$sampling_rate[1:5],
-    c(1.0000000, 0.9997893, 0.9985778, 0.9949431, 0.9881479),
+    c(1.0000000, 0.9997659, 0.9984197, 0.9943812, 0.9868310),
     tolerance = 0.0001
   )
 })
