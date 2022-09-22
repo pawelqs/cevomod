45383d776a5b44ffa9c36d65b7be5d92587fd2d1
Paweł <kpawel2210@gmail.com>
2022-09-22 14:50:50 +0200

    cumulative_tails now uses cevodata object

--- R/generics.R
+++ R/generics.R
@@ -22,6 +22,19 @@ add_SNV_data <- function(object, ...) {
   UseMethod("add_SNV_data")
 }
 
+#' @rdname assays
+#' @export
+CNVs <- function(object, ...) {
+  UseMethod("CNVs")
+}
+
+
+#' @rdname assays
+#' @export
+add_CNV_data <- function(object, ...) {
+  UseMethod("add_CNV_data")
+}
+
 
 #' Get/Set active assays of the cevodata object
 #' @param object object
@@ -41,20 +54,6 @@ default_SNVs <- function(object, ...) {
 #' @export
 `default_SNVs<-` <- function(object, ..., value) {
   UseMethod("default_SNVs<-")
-}
-
-
-#' @rdname assays
-#' @export
-CNVs <- function(object, ...) {
-  UseMethod("CNVs")
-}
-
-
-#' @rdname assays
-#' @export
-add_CNV_data <- function(object, ...) {
-  UseMethod("add_CNV_data")
 }
 
 
@@ -103,4 +102,38 @@ calc_SFS <- function(object, ...) {
 #' @export
 plot_SFS <- function(object, ...) {
   UseMethod("plot_SFS")
+}
+
+
+#' Cumulative tails
+#'
+#' Creates  cevodata$models$cumulative_tails tbl with the groupping variables and:
+#'   - n column with the number of mutations in the VAF interval
+#'   - x and y columns describing the cumulative tails
+#'   - y_scaled with y values scaled to the range 0-1
+#'
+#' @param object SNVs tibble object
+#' @param digits resolution of the cumulative tails calculation
+#' @param ... other arguments
+#' @examples
+#' data("tcga_brca_test")
+#' tcga_brca_test |>
+#'   calc_cumulative_tails()
+#'
+#' tcga_brca_test |>
+#'   plot_cumulative_tails()
+#' @name cumulative_tails
+
+
+#' @rdname cumulative_tails
+#' @export
+calc_cumulative_tails <- function(object, ...) {
+  UseMethod("calc_cumulative_tails")
+}
+
+
+#' @rdname cumulative_tails
+#' @export
+plot_cumulative_tails <- function(object, ...) {
+  UseMethod("plot_cumulative_tails")
 }
