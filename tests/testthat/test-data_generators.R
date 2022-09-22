ad7d880658cbf28a9b9c51786078b55c64a2d072
Paweł <kpawel2210@gmail.com>
2022-09-22 16:42:48 +0200

    tests fixed

--- tests/testthat/test-data_generators.R
+++ tests/testthat/test-data_generators.R
@@ -2,5 +2,9 @@ test_that("Data generator generates correct data", {
   snvs <- generate_neutral_snvs()
   expect_s3_class(snvs, "tbl_df")
   expect_equal(nrow(snvs), 1532)
-  expect_named(snvs, c("sample_id", "VAF", "n", "mut_id"))
+  expected_columns <- c(
+    "patient_id", "sample_id", "sample", "chrom", "pos", "gene_symbol", "ref",
+    "alt", "ref_reads", "alt_reads", "impact", "VAF", "n", "mut_id"
+  )
+  expect_named(snvs, expected_columns)
 })
