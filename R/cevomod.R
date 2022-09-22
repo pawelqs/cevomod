91e9e8b97df31bf0058b1a5c2c2abf4ff6d0cdc6
Paweł <kpawel2210@gmail.com>
2022-09-22 17:45:31 +0200

    snvs_test data removed

--- R/cevomod.R
+++ R/cevomod.R
@@ -1,21 +1,20 @@
 
+#' Tbl of driver genes from Bailey, Ding et al. 'Comprehensive Characterization
+#' of Cancer Driver Genes and Mutations', Cell, 2-18
+#' https://doi.org/10.1016/j.cell.2018.02.060
+#' @name driver_genes
+#' @docType data
+NULL
+
+#' Custom Variant Classification
+#' @name variant_classification
+#' @docType data
+NULL
+
 #' TCGA-BRCA SNVs dataset
 #' @name snvs_tcga_brca
 #' @docType data
 NULL
-
-
-#' Small SNV test dataset
-#' @name snvs_test
-#' @docType data
-NULL
-
-
-#' Small CNV test dataset
-#' @name cnvs_test
-#' @docType data
-NULL
-
 
 #' Small TCGA BRCA cevodata dataset
 #' @name tcga_brca_test
@@ -23,15 +22,4 @@ NULL
 NULL
 
 
-#' Tbl of driver genes from Bailey, Ding et al. 'Comprehensive Characterization of Cancer Driver
-#' Genes and Mutations', Cell, 2-18
-#' https://doi.org/10.1016/j.cell.2018.02.060
-#' @name driver_genes
-#' @docType data
-NULL
 
-
-#' Custom Variant Classification
-#' @name variant_classification
-#' @docType data
-NULL
--- data-raw/tcga_brca.R
+++ data-raw/tcga_brca.R
@@ -50,7 +50,6 @@ snvs_test <- snvs_tcga_brca %>%
   filter(sample_id %in% top_mutated_patients)
 class(snvs_tcga_brca) <- c("cevo_SNVs_tbl", class(snvs_test))
 
-usethis::use_data(snvs_test, overwrite = TRUE)
 
 cna_hg19 <- read_tsv("/mnt/dane/data/brca_tcga_pan_can_atlas_2018/data_cna_hg19.seg")
 cnvs_tcga_brca <- cna_hg19 |>
@@ -72,7 +71,6 @@ cnvs_tcga_brca <- cna_hg19 |>
 # usethis::use_data(cnvs_tcga_brca, overwrite = TRUE)
 cnvs_test <- cnvs_tcga_brca %>%
   filter(sample_id %in% top_mutated_patients)
-usethis::use_data(cnvs_test, overwrite = TRUE)
 
 
 tcga_brca_test <- init_cevodata("TCGA BRCA test data", genome = "hg37") |>
