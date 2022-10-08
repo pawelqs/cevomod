cfdbc3fbef6c01acca26c5fe9b428807cd16c07d
Paweł <kpawel2210@gmail.com>
2022-10-08 08:35:17 +0200

    Create run_cevomod_on_cevodatasets.R

---
+++ inst/run_cevomod_on_cevodatasets.R
@@ -0,0 +1,19 @@
+library(cevomod)
+library(cevoDatasets)
+library(furrr)
+
+
+plan(multisession, workers = 4)
+cli::cat_line("Running cevomod on cevoDatasets")
+
+data <- list(
+  AMLRO = AMLRO,
+  OPUS_BRCA = OPUS_BRCA,
+  OPUS_Larynx = OPUS_Larynx
+)
+data <- future_map(data, run_cevomod)
+
+dir.create("~/.cevomod", showWarnings = FALSE)
+readr::write_rds(data, "~/.cevomod/data.Rds")
+
+cli::cat_line("cevomod results saved to ~/.cevomod/data.Rds")
