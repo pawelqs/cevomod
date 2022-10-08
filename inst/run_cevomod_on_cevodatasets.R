00b4f8c37b1e5b8c505a35c4fc2338a7967da1a8
Paweł <kpawel2210@gmail.com>
2022-10-08 08:40:10 +0200

    Update run_cevomod_on_cevodatasets.R

--- inst/run_cevomod_on_cevodatasets.R
+++ inst/run_cevomod_on_cevodatasets.R
@@ -1,10 +1,9 @@
-library(cevomod)
-library(cevoDatasets)
-library(furrr)
-
-
+suppressPackageStartupMessages(library(cevomod))
+suppressPackageStartupMessages(library(cevoDatasets))
+suppressPackageStartupMessages(library(furrr))
 plan(multisession, workers = 4)
-cli::cat_line("Running cevomod on cevoDatasets")
+
+cli::cat_line("Running cevomod on cevoDatasets", col = "green4")
 
 data <- list(
   AMLRO = AMLRO,
@@ -16,4 +15,4 @@ data <- future_map(data, run_cevomod)
 dir.create("~/.cevomod", showWarnings = FALSE)
 readr::write_rds(data, "~/.cevomod/data.Rds")
 
-cli::cat_line("cevomod results saved to ~/.cevomod/data.Rds")
+cli::cat_line("cevomod results saved to ~/.cevomod/data.Rds", col = "green4")
