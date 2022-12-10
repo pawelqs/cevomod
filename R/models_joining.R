166b6a563f08e187f88c36840d0d24b8ce8073ba
Paweł <kpawel2210@gmail.com>
2022-12-10 23:52:50 +0100

    get_selected_mutations() in development

--- R/models_joining.R
+++ R/models_joining.R
@@ -1,4 +1,80 @@
 
+# Get selected mutations based on 2 sample models
+get_selected_mutations <- function(object, ...) {
+  # As for now for one patient only
+  if (!were_subclonal_models_fitted(object)) {
+    stop("Fit subclonal models first!")
+  }
+
+  samples_data <- object$metadata |>
+    select(sample_id:sample)
+  rowsample <- samples_data$sample[[1]]
+  colsample <- samples_data$sample[[2]]
+
+  mutations_mat <- object |>
+    get_2d_SNVs_matrix(rows_sample = rowsample, cols_sample = colsample, bins = 100)
+
+  intervals <- tibble(
+    interval = rownames(mutations_mat),
+    centers = get_interval_centers(interval)
+  )
+
+  VAF_zero_pred <- tibble(
+    VAF = 0, binom_pred = 0,
+    sample = samples_data$sample
+  )
+  binom_predictions <- get_residuals(object) |>
+    select(sample_id, VAF, binom_pred) |>
+    left_join(samples_data, by = "sample_id") |>
+    select(-sample_id)
+  binom_predictions <- VAF_zero_pred |>
+    bind_rows(binom_predictions) |>
+    pivot_wider(names_from = "sample", values_from = "binom_pred") |>
+    select(all_of(c("VAF", rowsample, colsample))) |>
+    set_names(c("VAF", "rowsample", "colsample"))
+
+  predictions_by_interval <- binom_predictions |>
+    rebinarize_distribution(VAFs = intervals$centers) |>
+    bind_cols(intervals)
+  row_predictions <- deframe(predictions_by_interval[, c("interval", "rowsample")])
+  col_predictions <- deframe(predictions_by_interval[, c("interval", "colsample")])
+
+  upper_limits <- mutations_mat
+  zero_interval <- rownames(mutations_mat)[[1]]
+  for (row in rownames(mutations_mat)) {
+    for (col in colnames(mutations_mat)) {
+      if (row == zero_interval && col == zero_interval) {
+        upper_limits[row, col] <- 0
+      } else if (row == zero_interval) {
+        upper_limits[row, col] <- min(mutations_mat[row, col], col_predictions[col] * 1.1)
+      } else if (col == zero_interval) {
+        upper_limits[row, col] <- min(mutations_mat[row, col], row_predictions[row] * 1.1)
+      } else {
+        upper_limits[row, col] <- min(mutations_mat[row, col], row_predictions[row] * 1.1, col_predictions[col] * 1.1)
+      }
+    }
+  }
+  upper_limits <- round(upper_limits)[1:5, 1:5]
+  # upper_limits[1:5, 1:5]
+
+  iter <- 2000
+  mc_arr <- array(
+    dim = c(nrow(mutations_mat), ncol(mutations_mat), iter),
+    dimnames = list(rownames(mutations_mat), colnames(mutations_mat), 1:iter)
+  )
+  for (row in rownames(mutations_mat)) {
+    for (col in colnames(mutations_mat)) {
+      mc_arr[row, col, ] <- round(runif(iter, max = upper_limits[row, col]))
+    }
+  }
+  mc_arr[, , 1]
+
+
+
+
+}
+
+
 get_2d_SNVs_matrix <- function(object,
                                rows_sample = NULL, cols_sample = NULL,
                                bins = 100) {
