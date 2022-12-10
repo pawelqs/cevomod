3abf4621dc952aab074f12a2690e6092b8737418
Paweł <kpawel2210@gmail.com>
2022-12-10 23:52:02 +0100

    get_2d_SNVs_matrix() implemented

---
+++ R/models_joining.R
@@ -0,0 +1,72 @@
+
+get_2d_SNVs_matrix <- function(object,
+                               rows_sample = NULL, cols_sample = NULL,
+                               bins = 100) {
+  patients_to_samples <- object$metadata |>
+    select(patient_id:sample)
+
+  if (is.null(rows_sample) || is.null(cols_sample)) {
+    rows_sample <- patients_to_samples$sample[[1]]
+    cols_sample <- patients_to_samples$sample[[2]]
+    message("Using '", rows_sample, "' as rows and '", cols_sample, "' as cols")
+  }
+  if (rows_sample %not in% patients_to_samples$sample) {
+    stop("Sample requested for rows is not present for this sample")
+  }
+  if (cols_sample %not in% patients_to_samples$sample) {
+    stop("Sample requested for cols is not present for this sample")
+  }
+
+  breaks <- c(-0.1, seq(0, 1, length.out = bins + 1))
+
+  # Prepare SNVs wider tibble
+  mutations <-get_SNVs_wider(object, fill_na = 0)
+  mutations <- mutations[c("mutation_id", rows_sample, cols_sample)]
+  colnames(mutations) <- c("mutation_id", "rows_sample", "cols_sample")
+
+  # Cut intervals
+  mutations <- mutations |>
+    mutate(across(where(is.double), ~cut(.x, breaks = breaks)))
+  intervals <- levels(mutations$rows_sample)
+
+  # Prepare square matrix
+  incomplete_mat <- mutations |>
+    mutate(across(c("rows_sample", "cols_sample"), as.character)) |>
+    group_by(rows_sample, cols_sample) |>
+    count() |>
+    ungroup() |>
+    pivot_wider(names_from = cols_sample, values_from = n) |>
+    column_to_rownames("rows_sample")
+  incomplete_mat[is.na(incomplete_mat)] <- 0
+
+  mat <- matrix(0, nrow = length(intervals), ncol = length(intervals))
+  colnames(mat) <- intervals
+  rownames(mat) <- intervals
+  mat[rownames(incomplete_mat), colnames(incomplete_mat)] <- as.matrix(incomplete_mat)
+
+  attr(mat, "rows_sample") <- rows_sample
+  attr(mat, "cols_sample") <- cols_sample
+  mat
+}
+
+
+get_interval_centers <- function(intervals) {
+  intervals |>
+    str_replace_all("[\\(\\)\\[\\]]", "") |>
+    str_split(pattern = ",") |>
+    map(parse_double) |>
+    map(mean) |>
+    unlist()
+}
+
+
+get_interval_width <- function(intervals) {
+  intervals |>
+    str_replace_all("[\\(\\)\\[\\]]", "") |>
+    str_split(pattern = ",") |>
+    map(parse_double) |>
+    map(~.x[[2]] - .x[[1]]) |>
+    unlist() |>
+    median()
+}
+
---
+++ tests/testthat/test-models_joining.R
@@ -0,0 +1,18 @@
+
+test_that("get_interval_centers() works", {
+  intervals <- (1:1000/1000) |>
+    cut_width(width = 0.1, center = 0.1) |>
+    levels()
+  res <- get_interval_centers(intervals)
+  expect_equal(res, 0:10/10)
+})
+
+
+test_that("get_interval_width() works", {
+  intervals <- (1:1000/1000) |>
+    cut_width(width = 0.1, center = 0.1) |>
+    levels()
+  res <- get_interval_width(intervals)
+  expect_equal(res, 0.1)
+})
+
