cea5872240626b5be9e663ab91c505ce8cbc7cba
Paweł <kpawel2210@gmail.com>
2023-02-06 09:58:39 +0100

    calc_mutation_frequencies.cevodata() exported

--- R/mutation_frequencies.R
+++ R/mutation_frequencies.R
@@ -13,9 +13,10 @@ calc_mutation_frequencies <- function(object, ...) {
 }
 
 
+#' @rdname mutation_frequencies
 #' @param object data object
 #' @param method method: "use_VAF". Other methods will be implemented later
-#' @rdname mutation_frequencies
+#' @export
 calc_mutation_frequencies.cevodata <- function(object, method = "use_VAF", ...) {
   if (method == "use_VAF") {
     snvs <- SNVs(object) |>
