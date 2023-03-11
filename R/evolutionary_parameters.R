24805fca18ff1c9c11e53b3b37f5b56007a8ce21
Paweł <kpawel2210@gmail.com>
2023-03-12 00:57:36 +0100

    get_selection_coefficients() implemented

--- R/evolutionary_parameters.R
+++ R/evolutionary_parameters.R
@@ -1,5 +1,7 @@
 
 
+## ------------------------ Mutation rates by Williams ------------------------
+
 #' Get mutation rates by Williams
 #' @param object cevodata object  with fitted cevomod models
 #' @param models_name models_name
@@ -33,66 +35,136 @@ get_mutation_rates <- function(object, models_name = "williams_neutral") {
 }
 
 
+
+## --------------------- Selection coefs by Williams --------------------------
+# Code below is re-formatted code from MOBSTER R package
+# by Caravagna, Williams et al. https://github.com/caravagnalab/mobster
+
+
 #' Get subclonal selection coefficients Williams
+#'
+#' Use properties of subclone fit to calculate selection intensity, selection
+#' is defined as the relative growth rates of host tumour cell
+#' populations (\eqn{\lambda h}) vs subclone (\eqn{\lambda s}):
+#' \deqn{1+s=\lambda h / \lambda s}
+#'
 #' @param object cevodata object
 #' @param Nmax Time when tumour is sampled (in tumour doublings)
 #' @export
 get_selection_coefficients <- function(object, Nmax = 10^10) {
   mutation_rates <- get_mutation_rates(object)
 
-  subclones <- get_models(object) |>
+  subclones <- get_models(object, "williams_neutral_subclones") |>
     filter(str_detect(.data$component, "Subclone")) |>
     drop_na_columns() |>
     select("sample_id", "component", "N_mutations", "cellularity") |>
-    mutate(cellularity = 2 * .data$cellularity)
+    mutate(subclone_frequency = 2 * .data$cellularity) |> # need ccf so times by 2
+    select(-"cellularity")
 
-  evolutionary_parameters <- subclones |>
+  dt <- subclones |>
     left_join(mutation_rates, by = "sample_id") |>
-    group_by(.data$sample_id) |>
     mutate(
-      subclones_in_sample = str_detect(.data$component, "Subclone") |> sum(),
-      emergence_time = get_emergence_time(.data$N_mutations, .data$mutation_rate_williams),
-      sequencing_time = get_sequencing_time(.data$subclones_in_sample, Nmax, .data$cellularity)
+      emergence_time = get_emergence_time(.data$N_mutations, .data$mutation_rate_williams)
     ) |>
-    rowwise() |>
-    mutate(
-      selection = calc_selection_coef(.data$emergence_time, .data$sequencing_time, .data$cellularity)
-    ) |>
-    ungroup() |>
-    select("sample_id", "component", "emergence_time", "sequencing_time", "selection")
+    nest(subclones = c("component", "N_mutations", "subclone_frequency", "emergence_time"))
 
-  evolutionary_parameters
+  dt |>
+    rowwise("sample_id") |>
+    reframe(mobster_evolutionary_parameters(subclones, mu))
 }
 
 
+# Emergence time is negative, when N is not big enough, eg. comparable with mu
 get_emergence_time <- function(N, mu) {
   (N / mu) / (2 * log(2)) - (-digamma(1) / log(2))
 }
 
 
-get_sequencing_time <- function(n_subclones, N_max, cellularity) {
-  if_else(
-    n_subclones == 1,
-    log(N_max * (1 - cellularity)) / log(2),
-    NA_real_
-  )
+mobster_evolutionary_parameters <- function(subclones, mu, Nmax = 10^10) {
+  nsubclones <- nrow(subclones)
+
+  subclones$selection <- if (nsubclones == 1) {
+    time_end <- log(Nmax * (1 - subclones$subclone_frequency)) / log(2)
+    selection(subclones$emergence_time, time_end, subclones$subclone_frequency)
+  } else if (nsubclones == 2) {
+    if (are_subclones_nested(subclones)) { # pigeon hole principle
+      largestsubclone <- max(subclones$subclone_frequency)
+      time_end <- log(Nmax * (1 - largestsubclone)) / log(2)
+      selection2clonenested(subclones$emergence_time, time_end, subclones$subclone_frequency)
+    }
+    else {
+      time_end <- log(Nmax * ( 1 - subclones$subclone_frequency[1] - subclones$subclone_frequency[2] )) / log(2)
+      selection2clone(subclones$emergence_time, time_end, subclones$subclone_frequency)
+    }
+  }
+  return(subclones)
 }
 
 
-# Calculate strength of selection of subclone (from MOBSTER)
-#
-# Use properties of subclone fit to calculate selection intensity, selection
-# is defined as the relative growth rates of host tumour cell
-# populations (\eqn{\lambda h}) vs subclone (\eqn{\lambda s}):
-# \deqn{1+s=\lambda h / \lambda s}
-#
+are_subclones_nested <- function(subclones) {
+  sum(subclones$subclone_frequency) > 1
+}
+
+
+# Calculate strength of selection of subclone
 # @param time time subclone emerges (in tumour doublings)
 # @param time_end Time when tumour is sampled (in tumour doublings)
 # @param subclonefrequency Frequency of subclones
-calc_selection_coef <- function(time, time_end, subclonefrequency) {
+selection <- function(time, time_end, subclonefrequency) {
   x1 <- log(2) * time
   x2 <- log(subclonefrequency / (1 - subclonefrequency))
   x3 <- log(2) * (time_end - time)
+
   s <- ((x1 + x2) / x3)
-  s
+  return(s)
 }
+
+
+# Calculate strength of selection for 2 independent subclones
+# @param times emergence times (in tumour doublings)
+# @param time_end Time when tumour is sampled (in tumour doublings)
+# @param frequencies Frequencies of both subclones
+selection2clone <- function(times, time_end, frequencies) {
+  time1 <- times[1]
+  time2 <- times[2]
+  subclonefrequency1 <- frequencies[1]
+  subclonefrequency2 <- frequencies[2]
+
+  x1 <- log(2) * time1
+  x2 <-
+    log(subclonefrequency1 / (1 - subclonefrequency1 - subclonefrequency2))
+  x3 <- log(2) * (time_end - time1)
+  s1 <- ((x1 + x2) / x3)
+
+  x1 <- log(2) * time2
+  x2 <-
+    log(subclonefrequency2 / (1 - subclonefrequency1 - subclonefrequency2))
+  x3 <- log(2) * (time_end - time2)
+  s2 <- ((x1 + x2) / x3)
+
+  return(c(s1, s2))
+}
+
+
+# Calculate strength of selection for 2 nested subclones
+selection2clonenested <- function(times, time_end, frequencies) {
+  time1 <- times[1]
+  time2 <- times[2]
+  subclonefrequency1 <- frequencies[1]
+  subclonefrequency2 <- frequencies[2]
+
+  x1 <- log(2) * time1
+  x2 <- log((subclonefrequency1 - subclonefrequency2)
+            / (1 - subclonefrequency1))
+  x3 <- log(2) * (time_end - time1) # I replaced time with time1, I am sure it was a typo; PK
+  s1 <- ((x1 + x2) / x3)
+
+  x1 <- log(2) * time2
+  x2 <- log((subclonefrequency2)
+            / (1 - subclonefrequency1))
+  x3 <- log(2) * (time_end - time2)
+  s2 <- ((x1 + x2) / x3)
+
+  return(c(s1, s2))
+}
+
