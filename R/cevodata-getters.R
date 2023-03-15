be6141ca431b67c92e9f9231f194dddbb4b003ef
Paweł <kpawel2210@gmail.com>
2023-03-15 22:53:12 +0100

    getters moved to one file

--- R/cevodata-getters.R
+++ R/cevodata-getters.R
@@ -133,6 +133,78 @@ get_SNVs_wider_intervals <- function(object, fill_na = NULL, bins = NULL) {
 }
 
 
+## ------------------------------- Models ------------------------------------
+
+
+#' Get model names
+#' @param object object
+#' @export
+get_model_names <- function(object) {
+  UseMethod("get_model_names")
+}
+
+
+#' @export
+get_model_names.cevodata <- function(object) {
+  names(object$models)
+}
+
+
+#' Get models from the object
+#' @param object object to get the models from
+#' @param ... other arguments
+#' @export
+get_models <- function(object, ...) {
+  UseMethod("get_models")
+}
+
+
+#' @describeIn get_models Get models from cevodata object
+#' @param which `chr` which models to get
+#' @param best_only `lgl` return only the best models?
+#' @export
+get_models.cevodata <- function(object,
+                                which = active_models(object),
+                                best_only = TRUE,
+                                ...) {
+  models <- object$models[[which]]
+  models_class <- class(models)
+  if (is.null(models)) {
+    stop("Slot ", which, " is empty! Fit apropriate model first")
+  }
+  if (best_only && !is.null(models[["best"]])) {
+    models <- filter(models, .data$best)
+    class(models) <- models_class
+    models
+  } else {
+    models
+  }
+}
+
+
+get_powerlaw_models <- function(object,
+                                which = active_models(object),
+                                best_only = TRUE,
+                                ...) {
+  models <- get_models(object, which)
+  if ("cevo_powerlaw_models" %not in% class(models)) {
+    stop(
+      which, " is not a powerlaw model, required to fit subclones.",
+      "Use another model"
+    )
+  }
+  models
+}
+
+
+active_models <- function(object, ...) {
+  if (is.null(object$active_models) | length(object$models) == 0) {
+    stop("No models has been fitted yet!")
+  }
+  object$active_models
+}
+
+
 ## ---------------------------------- Other -----------------------------------
 
 #' @rdname assays
