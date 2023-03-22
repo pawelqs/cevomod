318db5751a0ee8245b2051a0495ce35ab09c8c48
Paweł <kpawel2210@gmail.com>
2023-03-22 13:43:08 +0100

    plot_sequencing_depth.cevo_snvs() added

--- R/plots_data_overview.R
+++ R/plots_data_overview.R
@@ -1,7 +1,9 @@
 
 #' Plot sequencing depth of cevodata
-#' @param object cevodata
-#' @param ... other params
+#' @param object cevodata or cevo_snvs
+#' @param snvs_name name of the SNVs table
+#' @param geom change geom
+#' @param ... params passed to geom
 #' @export
 plot_sequencing_depth <- function(object, ...) {
   UseMethod("plot_sequencing_depth")
@@ -9,25 +11,28 @@ plot_sequencing_depth <- function(object, ...) {
 
 
 #' @describeIn plot_sequencing_depth Plot sequencing data of mutations in cevodata
-#' @param mapping customize mapping
-#' @param geom change geom
-#' @param ... params passed to geom
 #' @export
 plot_sequencing_depth.cevodata <- function(object,
-                                           mapping = NULL,
+                                           snvs_name = default_SNVs(object),
                                            geom = geom_boxplot,
                                            ...) {
-  snvs <- SNVs(object) |>
+  snvs <- SNVs(object, snvs_name) |>
     left_join(object$metadata, by = "sample_id")
-  samples_order <- snvs |>
+  plot_sequencing_depth(snvs, geom, ...)
+}
+
+
+#' @describeIn plot_sequencing_depth Plot sequencing data of mutations in cevodata
+#' @export
+plot_sequencing_depth.cevo_snvs <- function(object, geom = geom_boxplot, ...) {
+  samples_order <- object |>
     group_by(.data$sample_id) |>
     summarise(median_dp = stats::median(.data$DP)) |>
     arrange(desc(.data$median_dp)) |>
     pull("sample_id")
-  snvs <- snvs |>
+  object <- object |>
     mutate(sample_id = parse_factor(.data$sample_id, levels = samples_order))
-  default_mapping <- aes(.data$sample_id, .data$DP, fill = .data$sample_id)
-  ggplot(snvs, join_aes(default_mapping, mapping)) +
+  ggplot(object, aes(.data$sample_id, .data$DP, fill = .data$sample_id)) +
     geom(...) +
     scale_y_log10() +
     theme_minimal() +
@@ -51,10 +56,7 @@ plot_private_shared_mutations <- function(object, ...) {
 #' @describeIn plot_private_shared_mutations Plot private and shared mutation fractions
 #' @inheritParams plot_sequencing_depth
 #' @export
-plot_private_shared_mutations.cevodata <- function(object,
-                                                   mapping = NULL,
-                                                   geom = geom_bar,
-                                                   ...) {
+plot_private_shared_mutations.cevodata <- function(object, geom = geom_bar, ...) {
   if (is.null(object$metadata$patient_id)) {
     stop("patient_id column must be present to plot this chart!")
   }
@@ -67,8 +69,7 @@ plot_private_shared_mutations.cevodata <- function(object,
     count() |>
     mutate(n = as.character(.data$n))
 
-  default_mapping <- aes(.data$patient_id, fill = .data$n)
-  ggplot(plot_data, join_aes(default_mapping, mapping)) +
+  ggplot(plot_data, aes(.data$patient_id, fill = .data$n)) +
     geom(position = "fill", ...) +
     theme_minimal() +
     labs(
