209e18ccec23ab917d4dda3849e7bd42501263a6
Paweł <kpawel2210@gmail.com>
2023-05-04 12:09:41 +0200

    test_data_fitted update

--- data-raw/test_data.R
+++ data-raw/test_data.R
@@ -5,10 +5,14 @@ test_data
 use_data(test_data, overwrite = TRUE)
 
 
-test_data_fitted <- test_data |>
-  prepare_SNVs() |>
-  fit_powerlaw_tail_fixed() |>
-  fit_subclones() |>
-  fit_powerlaw_tail_optim() |>
-  fit_subclones()
+withr::with_seed(
+  123,
+  test_data_fitted <- test_data |>
+    prepare_SNVs() |>
+    fit_powerlaw_tail_fixed() |>
+    fit_subclones() |>
+    fit_powerlaw_tail_optim() |>
+    fit_subclones()
+)
+
 use_data(test_data_fitted, overwrite = TRUE)
