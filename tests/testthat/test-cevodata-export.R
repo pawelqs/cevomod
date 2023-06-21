7f5bed8605e710046d4fa3384247cf5313ee8d0a
Paweł <kpawel2210@gmail.com>
2023-06-21 11:34:19 +0200

    to_clip() implemented

--- tests/testthat/test-cevodata-export.R
+++ tests/testthat/test-cevodata-export.R
@@ -1,32 +1,26 @@
+cd <- test_data
+new_cnvs <- CNVs(cd) |>
+  filter(sample_id != "Sample 3")
+cd <- cd |>
+  add_CNV_data(new_cnvs, "new_cnvs")
+res <- to_clip(cd)
 
-test_that("CliP export works", {
-  cd <- test_data
-  new_cnvs <- CNVs(cd) |>
-    filter(sample_id != "Sample 3")
-  cd <- cd |>
-    add_CNV_data(new_cnvs, "new_cnvs")
 
-  res <- to_clip(cd)
-
+test_that("to_clip() creates correct tibbles for each sample", {
   expect_named(res, str_c("Sample ", 1:4))
   res |>
-    map_lgl(\(x) all(names(x) == c("snvs", "cnvs", "purities"))) |>
-    all() |>
-    expect_true()
-  res |>
-    map("snvs") |>
-    map_lgl(is_tibble) |>
-    all() |>
-    expect_true()
-  res |>
-    map("snvs") |>
-    map_lgl(is_tibble) |>
-    all() |>
-    expect_true()
-  res |>
-    map("purities") |>
-    map_lgl(is.double) |>
+    map(\(x) names(x) == c("snvs", "cnvs", "purities")) |>
+    map_lgl(all) |>
     all() |>
     expect_true()
 })
 
+
+test_that("to_clip() creates output files with correct column names", {
+  expect_named(res$`Sample 1`$snvs, c("chromosome_index", "position", "alt_count", "ref_count"))
+  expect_named(
+    res$`Sample 1`$cnvs,
+    c("chromosome_index", "start_position", "end_position", "major_cn", "minor_cn", "total_cn")
+  )
+  expect_type(res$`Sample 1`$purities, "double")
+})
