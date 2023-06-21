0efa95c25b303c07e6def5eb705dcf6289360e0d
Paweł <kpawel2210@gmail.com>
2023-06-21 10:49:40 +0200

    annotate_normal_cn() implemented

---
+++ tests/testthat/test-cevo_cnvs.R
@@ -0,0 +1,18 @@
+cnvs <- tibble(
+  sample_id = c("A", "A", "B", "B", "C", "C"),
+  chrom = c("ch1", "chrX", "chr1", "chrY", "chr1", "chrX"),
+  start = 1,
+  end = 100
+)
+cd <- init_cevodata("test", cnvs = cnvs)
+cd$metadata$sex <- c("M", "male", "female")
+
+
+test_that("annotate_normal_cn() works", {
+  res <- cd |>
+    annotate_normal_cn() |>
+    CNVs()
+  expected <- c(2, 1, 2, 1, 2, 2)
+  expect_true("normal_cn" %in% names(res))
+  expect_equal(res$normal_cn, expected)
+})
