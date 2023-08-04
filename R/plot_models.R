9bc6a261cc5cf1ef89f980acff4a226a7310bf8d
Paweł <kpawel2210@gmail.com>
2023-08-04 11:15:04 +0200

    Plot models can plot bootstrapped powerlaw models

--- R/plot_models.R
+++ R/plot_models.R
@@ -10,6 +10,7 @@
 #' @name plot_models
 
 
+
 #' @rdname plot_models
 #' @export
 plot_models <- function(object, ...) {
@@ -17,14 +18,14 @@ plot_models <- function(object, ...) {
 }
 
 
+
 #' @rdname plot_models
-#' @param neutral_tail_alpha 0.3
-#' @param neutral_tail_size 0.5
-#' @param neutral_tail_fill "white"
-#' @param neutral_tail_color "gray90"
-#' @param binomial_layer_color "black"
-#' @param final_fit_color "red"
-#' @param final_fit_size 1
+#' @param params_neutral_tail List of non-default params for neutral tail geom
+#' @param params_bootstraps List of non-default params for neutral tail bootstraps
+#'   geom
+#' @param params_binomial List of non-default params for binomial geom
+#' @param params_subclones List of non-default params for subclones geom
+#' @param params_final_fit List of non-default params for final fit geom
 #' @param nrow passed to facet_wrap
 #' @param ncol passed to facet_wrap
 #' @export
@@ -34,73 +35,38 @@ plot_models.cevodata <- function(object,
                                  show_binomial_layer = FALSE,
                                  show_subclones = TRUE,
                                  show_final_fit = TRUE,
-                                 neutral_tail_alpha = 0.3,
-                                 neutral_tail_size = 0.5,
-                                 neutral_tail_fill = "white",
-                                 neutral_tail_color = "gray90",
-                                 binomial_layer_color = "black",
-                                 final_fit_color = "red",
-                                 final_fit_size = 1,
+                                 params_neutral_tail = list(),
+                                 params_bootstraps = list(),
+                                 params_binomial = list(),
+                                 params_subclones = list(),
+                                 params_final_fit = list(),
                                  nrow = NULL, ncol = NULL,
                                  ...) {
-
   models <- get_models(object, models_name)
   neutral_lm_fitted <- "alpha" %in% names(models)
   subclones_fitted <- "cellularity" %in% names(models)
+  bootstraped <- "cevo_bootstrap_powerlaw_models" %in% class(models)
 
   resid <- get_residuals(object, models_name) |>
     left_join(object$metadata, by = "sample_id") |>
     mutate(sample_id = parse_factor(.data$sample_id, levels = object$metadata$sample_id)) |>
-    group_by(.data$sample_id) |>
-    mutate(
-      ylim = max(.data$SFS) * 1.2,
-      powerlaw_pred = case_when(
-        .data$powerlaw_pred > .data$ylim ~ Inf,
-        .data$f < 0 ~ Inf,
-        TRUE ~ .data$powerlaw_pred
-      )
-    ) |>
-    ungroup()
+    trim_powerlaw_pred()
 
   model_layers <- list(
-    if (show_neutral_tail && neutral_lm_fitted) {
-      geom_area(
-        aes(.data$f, .data$powerlaw_pred),
-        data = resid,
-        fill = neutral_tail_fill, color = neutral_tail_color,
-        alpha = neutral_tail_alpha,
-        size = neutral_tail_size, show.legend = FALSE,
-        stat = "identity"
-      )
+    if (show_neutral_tail && neutral_lm_fitted && !bootstraped) {
+      geom_area_neutral_tail(resid, params_neutral_tail)
+    },
+    if (show_neutral_tail && neutral_lm_fitted && bootstraped) {
+      geom_line_bootstraps(resid, params_bootstraps)
     },
     if (show_binomial_layer && subclones_fitted) {
-      geom_line(
-        aes(.data$f, .data$binom_pred),
-        data = resid,
-        size = 1, color = binomial_layer_color, linetype = "dashed"
-      )
+      geom_line_binomial(resid, params_binomial)
     },
     if (show_subclones && subclones_fitted) {
-      dt <- resid |>
-        pivot_longer(
-          cols = c("Clone", starts_with("Subclone")),
-          names_to = "component",
-          values_to = "pred"
-        ) |>
-        filter(!is.na(.data$pred))
-      geom_area(
-        aes(.data$f, .data$pred, group = .data$component),
-        data = dt,
-        position = "identity",
-        size = 1, alpha = 0.3, color = "black", show.legend = FALSE
-      )
+      geom_are_subclones(resid, params_subclones)
     },
     if (show_final_fit && neutral_lm_fitted && subclones_fitted) {
-      geom_line(
-        aes(.data$f, .data$model_pred),
-        data = resid |> filter(.data$powerlaw_pred < .data$ylim),
-        size = final_fit_size, color = final_fit_color
-      )
+      geom_line_final_fit(resid, params_final_fit)
     }
   )
 
@@ -108,6 +74,100 @@ plot_models.cevodata <- function(object,
     model_layers +
     facet_wrap(~.data$sample_id, scales = "free_y", nrow = nrow, ncol = ncol)
 }
+
+
+
+trim_powerlaw_pred <- function(resid, limit = 1.2) {
+  resid |>
+    group_by(.data$sample_id) |>
+    mutate(
+      ylim = max(.data$SFS) * limit,
+      powerlaw_pred = case_when(
+        .data$powerlaw_pred > .data$ylim ~ Inf,
+        .data$f < 0 ~ Inf,
+        TRUE ~ .data$powerlaw_pred
+      )
+    ) |>
+    ungroup()
+}
+
+
+geom_area_neutral_tail <- function(resid, params = list()) {
+  geom_area(
+    aes(.data$f, .data$powerlaw_pred),
+    data = resid,
+    fill = if (is.null(params$fill)) "white" else params$fill,
+    color = if (is.null(params$color)) "gray90" else params$color,
+    alpha = if (is.null(params$alpha)) 0.3 else params$alpha,
+    size = if (is.null(params$size)) 0.5 else params$size,
+    show.legend = FALSE,
+    stat = "identity"
+  )
+}
+
+
+
+geom_line_bootstraps <- function(resid, params = list()) {
+  geom_line(
+    aes(.data$f, .data$powerlaw_pred, group = .data$resample_id),
+    data = resid,
+    alpha = if (is.null(params$alpha)) 1 else params$alpha,
+    color = if (is.null(params$color)) "white" else params$color,
+    linetype = if (is.null(params$linetype)) "solid" else params$linetype,
+    linewidth = if (is.null(params$linewidth)) 0.3 else params$linewidth,
+    show.legend = FALSE,
+    stat = "identity"
+  )
+}
+
+
+
+geom_line_binomial <- function(resid, params = list()) {
+  geom_line(
+    aes(.data$f, .data$binom_pred),
+    data = resid,
+    alpha = if (is.null(params$alpha)) 1 else params$alpha,
+    color = if (is.null(params$color)) "black" else params$color,
+    linetype = if (is.null(params$linetype)) "dashed" else params$linetype,
+    linewidth = if (is.null(params$linewidth)) 1 else params$linewidth,
+  )
+}
+
+
+
+geom_are_subclones <- function(resid, params) {
+  dt <- resid |>
+    pivot_longer(
+      cols = c("Clone", starts_with("Subclone")),
+      names_to = "component",
+      values_to = "pred"
+    ) |>
+    filter(!is.na(.data$pred))
+  geom_area(
+    aes(.data$f, .data$pred, group = .data$component),
+    data = dt,
+    position = "identity",
+    alpha = if (is.null(params$alpha)) 0.3 else params$alpha,
+    color = if (is.null(params$color)) "black" else params$color,
+    linetype = if (is.null(params$linetype)) "solid" else params$linetype,
+    linewidth = if (is.null(params$linewidth)) 1 else params$linewidth,
+    show.legend = FALSE
+  )
+}
+
+
+
+geom_line_final_fit <- function(resid, params) {
+  geom_line(
+    aes(.data$f, .data$model_pred),
+    data = resid |> filter(.data$powerlaw_pred < .data$ylim),
+    alpha = if (is.null(params$alpha)) 1 else params$alpha,
+    color = if (is.null(params$color)) "red" else params$color,
+    linetype = if (is.null(params$linetype)) "solid" else params$linetype,
+    linewidth = if (is.null(params$linewidth)) 1 else params$linewidth
+  )
+}
+
 
 
 plot_clones <- function(clones) {
--- tests/testthat/test-plot_models.R
+++ tests/testthat/test-plot_models.R
@@ -2,6 +2,57 @@ data("tcga_brca_test")
 set_cevomod_verbosity(0)
 
 
+test_that("plot_models() works for powerlaw_optim_subclones", {
+  models_name <- "powerlaw_optim_subclones"
+  show_neutral_tail <- TRUE
+  show_binomial_layer <- FALSE
+  show_subclones <- TRUE
+  show_final_fit <- TRUE
+  neutral_tail_alpha <- 0.3
+  neutral_tail_size <- 0.5
+  neutral_tail_fill <- "white"
+  neutral_tail_color <- "red"#"gray90"
+  binomial_layer_color <- "black"
+  final_fit_color <- "red"
+  final_fit_size <- 1
+  nrow <- NULL
+  ncol <- NULL
+
+  suppressWarnings({
+    p <- plot_models(test_data_fitted, models_name = "powerlaw_optim_subclones")
+  })
+  expect_s3_class(p, c("gg", "ggplot"))
+  vdiffr::expect_doppelganger("powerlaw_optim_subclones", p)
+})
+
+
+
+test_that("plot_models() works for bs_powerlaw_model_bootstraps", {
+  models_name <- "bs_powerlaw_model_bootstraps"
+  show_neutral_tail <- TRUE
+  show_binomial_layer <- FALSE
+  show_subclones <- TRUE
+  show_final_fit <- TRUE
+  neutral_tail_alpha <- 0.3
+  neutral_tail_size <- 0.5
+  neutral_tail_fill <- "white"
+  neutral_tail_color <- "red"#"gray90"
+  binomial_layer_color <- "black"
+  final_fit_color <- "red"
+  final_fit_size <- 1
+  nrow <- NULL
+  ncol <- NULL
+
+
+  suppressWarnings({
+    p <- plot_models(test_data_fitted, models_name = "bs_powerlaw_model_bootstraps")
+  })
+  expect_s3_class(p, c("gg", "ggplot"))
+  vdiffr::expect_doppelganger("bs_powerlaw_model_bootstraps", p)
+})
+
+
+
 test_that("Comparing multiple models works", {
   object <- tcga_brca_test |>
     fit_powerlaw_tail_fixed() |>
@@ -13,5 +64,3 @@ test_that("Comparing multiple models works", {
   })
   expect_s3_class(p, c("gg", "ggplot"))
 })
-
-
