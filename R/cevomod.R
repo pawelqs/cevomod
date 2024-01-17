280a8d392971270bc304ee21195fee310a206053
Paweł <kpawel2210@gmail.com>
2024-01-17 14:43:38 +0100

    Documentation update

--- R/cevomod.R
+++ R/cevomod.R
@@ -1,18 +1,7 @@
 
-#' Tbl of driver genes from Bailey, Ding et al. 'Comprehensive Characterization
-#' of Cancer Driver Genes and Mutations', Cell, 2-18
-#' https://doi.org/10.1016/j.cell.2018.02.060
-#' @name driver_genes
-#' @docType data
-NULL
-
-#' Custom Variant Classification
-#' @name variant_classification
-#' @docType data
-NULL
 
 #' Small TCGA BRCA cevodata dataset
-#' @name tcga_brca_test
+#' @name tcga_brca_fitted
 #' @docType data
 NULL
 
